bd6435a48c59c96534ac9607f4d97ec77eadad5c
Julia Ivanova <for.julia96@gmail.com>
2022-08-06 21:10:57 +0300

    Handle general errors

--- src/main/java/com/rest/rest/StudentRestController.java
+++ src/main/java/com/rest/rest/StudentRestController.java
@@ -56,4 +56,18 @@ public class StudentRestController {
     // return ResponseEntity
     return new ResponseEntity(error, HttpStatus.NOT_FOUND);
   }
+
+  // add another exception handler ... to catch any exception
+  @ExceptionHandler
+  public ResponseEntity<StudentNotFoundException> handleException (Exception exc) {
+    // create a StudentErrorResponse
+    StudentErrorResponse error = new StudentErrorResponse();
+
+    error.setStatus(HttpStatus.BAD_REQUEST.value());
+    error.setMessage(exc.getMessage());
+    error.setTimestamp(System.currentTimeMillis());
+
+    // return ResponseEntity
+    return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+  }
 }
