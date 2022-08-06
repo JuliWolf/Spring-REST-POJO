201985d90ff8e82acc50940018c52cc3e2bd255f
Julia Ivanova <for.julia96@gmail.com>
2022-08-06 17:24:43 +0300

    Generate students and return in rest

---
+++ src/main/java/com/rest/rest/StudentRestController.java
@@ -0,0 +1,26 @@
+package com.rest.rest;
+
+import com.rest.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+  // define endpoint for "/students" - return list of students
+  @GetMapping("/students")
+  public List<Student> getStudents () {
+    List<Student> students = new ArrayList<>();
+
+    students.add(new Student("Mary", "Pate"));
+    students.add(new Student("John", "Black"));
+    students.add(new Student("Nick", "Mitchel"));
+
+    return students;
+  }
+}
