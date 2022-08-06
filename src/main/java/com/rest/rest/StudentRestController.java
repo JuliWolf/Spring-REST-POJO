c36521510b7ef890371d23ae41583563c7867b70
Julia Ivanova <for.julia96@gmail.com>
2022-08-06 21:19:43 +0300

    Use ControllerAdvice

--- src/main/java/com/rest/rest/StudentRestController.java
+++ src/main/java/com/rest/rest/StudentRestController.java
@@ -42,32 +42,4 @@ public class StudentRestController {
 
     return students.get(studentId);
   }
-
-  // Add an exception handler using @ExceptionHandler
-  @ExceptionHandler
-  public ResponseEntity<StudentNotFoundException> handleException (StudentNotFoundException exc) {
-    // create a StudentErrorResponse
-    StudentErrorResponse error = new StudentErrorResponse();
-
-    error.setStatus(HttpStatus.NOT_FOUND.value());
-    error.setMessage(exc.getMessage());
-    error.setTimestamp(System.currentTimeMillis());
-
-    // return ResponseEntity
-    return new ResponseEntity(error, HttpStatus.NOT_FOUND);
-  }
-
-  // add another exception handler ... to catch any exception
-  @ExceptionHandler
-  public ResponseEntity<StudentNotFoundException> handleException (Exception exc) {
-    // create a StudentErrorResponse
-    StudentErrorResponse error = new StudentErrorResponse();
-
-    error.setStatus(HttpStatus.BAD_REQUEST.value());
-    error.setMessage(exc.getMessage());
-    error.setTimestamp(System.currentTimeMillis());
-
-    // return ResponseEntity
-    return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
-  }
 }
