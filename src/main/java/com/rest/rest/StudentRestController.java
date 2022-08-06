ee503b0f6c9b758664dfb2f5318c447b5fa6435a
Julia Ivanova <for.julia96@gmail.com>
2022-08-06 17:38:08 +0300

    get student by index using query parameters

--- src/main/java/com/rest/rest/StudentRestController.java
+++ src/main/java/com/rest/rest/StudentRestController.java
@@ -2,9 +2,11 @@ package com.rest.rest;
 
 import com.rest.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,15 +14,27 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-  // define endpoint for "/students" - return list of students
-  @GetMapping("/students")
-  public List<Student> getStudents () {
-    List<Student> students = new ArrayList<>();
+  private List<Student> students;
+
+  // define @PostConstruct to load the student fata ... only once
+  @PostConstruct
+  public void loadData () {
+    students = new ArrayList<>();
 
     students.add(new Student("Mary", "Pate"));
     students.add(new Student("John", "Black"));
     students.add(new Student("Nick", "Mitchel"));
+  }
 
+  // define endpoint for "/students" - return list of students
+  @GetMapping("/students")
+  public List<Student> getStudents () {
     return students;
+  }
+
+  // define endpoint for "/students/{studentId}" - return student at index
+  @GetMapping("/students/{studentId}")
+  public Student getStudent (@PathVariable int studentId) {
+    return students.get(studentId);
   }
 }
