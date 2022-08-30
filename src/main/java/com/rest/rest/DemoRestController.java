d0acacade5aa31a5b6f90cdfe0570d3ba575b2fa
Julia Ivanova <for.julia96@gmail.com>
2022-08-30 16:59:05 +0300

    use application properties in REST

--- src/main/java/com/rest/rest/DemoRestController.java
+++ src/main/java/com/rest/rest/DemoRestController.java
@@ -1,5 +1,6 @@
 package com.rest.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,4 +12,18 @@ public class DemoRestController {
   public String sayHello () {
     return "Hello World!";
   }
+
+  // inject properties for: coach.name and team.name
+  @Value("${coach.name}")
+  private String coachName;
+
+  @Value("${team.name}")
+  private String teamName;
+
+  // expose new endpoint for "teaminfo"
+  @GetMapping("/teaminfo")
+  public String getTeamInfo () {
+    return "Coach: " + coachName + ", Team name: " + teamName;
+  }
+
 }
